c4ff0a67b0108c3f83b246132639cd672d6adc24
linhvuhoang <redtonlinh@gmail.com>
2019-10-31 19:50:24 +0700

    baithi 2
    bai 2

---
+++ baithi2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+	float sum, rate ,interest;
+	int time;
+	printf("nhap von\n");
+	scanf("%f",&sum);
+	printf("nhap lai \n");
+	scanf("%f",&rate);
+	printf("nhap nam\n");
+	scanf("%d",&time);
+	for(int i = 0 ; i < time ; i ++){
+		printf("Year\tInterest\tSum\n");
+		interest = sum*rate/100;
+		sum += interest;
+		printf("%d\t%f\t%f\n",i+1,interest,sum);
+	}
+	return 0;
+}
